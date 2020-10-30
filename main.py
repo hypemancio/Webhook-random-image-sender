6d1c80fab652fbcc324e07bb64fd6a3b4703af47
hypemancio <44851276+hypemancio@users.noreply.github.com>
2020-10-30 20:34:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,21 +7,27 @@ with open('config.json', 'r') as handle:
 interval = (config["interval"])
 hook = Webhook((config["webhook"]))
 loop = (config["loop"])
-if not os.path.exists('.\\images\\'):
-    print ("Creating folder: images.")
-    os.makedirs('.\\images\\')
+path = "./images"
+
+if not os.path.exists('./images'):
+    try:
+        os.mkdir('./images')
+    except OSError:
+        print ('Creation of the directory {} failed').format(path))
+    else:
+        print('Successfully created the directory {}'.format(path))
     
 def forever():
     print ("Sending images every " + str(interval) + " seconds.")
     while True:
         try:
-            file = random.choice(os.listdir(".\\images\\"))
+            file = random.choice(os.listdir("./images"))
         except Exception:
             print ("There are no files in the images folder. Please add some.")
             time.sleep(5)
             sys.exit()
         print (file)
-        file = File(".\\images\\" + str(file))
+        file = File("./images/" + str(file))
         hook.send(file=file)
         time.sleep(int(interval))
 
